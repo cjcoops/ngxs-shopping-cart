8845848c55f725c53e49fe83aa4e548426b3ccb7
Chris Cooper <cjcooper252@gmail.com>
2018-10-08 20:53:23 -0700

    creates shop module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { Routes, RouterModule } from "@angular/router";
+
+export const ROUTES: Routes = [
+  { path: "", pathMatch: "full", redirectTo: "shop" },
+  {
+    path: "shop",
+    loadChildren: "./shop/shop.module#ShopModule"
+  }
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, RouterModule.forRoot(ROUTES)],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
