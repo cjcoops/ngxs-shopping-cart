a81bcbba9deb989f65d03f87684d38fd0c608e57
Chris Cooper <cjcooper252@gmail.com>
2018-10-10 22:15:15 -0700

    adds single product page

---
+++ src/app/shop/components/products/single-product/single-product.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from "@angular/core";
+import { Product } from "../../../models/product.model";
+
+@Component({
+  selector: "app-single-product",
+  templateUrl: "./single-product.component.html",
+  styleUrls: ["./single-product.component.css"]
+})
+export class SingleProductComponent implements OnInit {
+  product: Product = {
+    id: 1,
+    name: "Red Apple",
+    price: 0.4,
+    emoji: "🍎"
+  };
+
+  constructor() {}
+
+  ngOnInit() {}
+}
--- src/app/shop/shop.module.ts
+++ src/app/shop/shop.module.ts
@@ -5,21 +5,31 @@ import { Routes, RouterModule } from "@angular/router";
 import { ProductsComponent } from "./components/products/products.component";
 import { CartComponent } from "./components/cart/cart.component";
 import { ShopService } from "./services/shop.service";
+import { SingleProductComponent } from "./components/products/single-product/single-product.component";
 
 export const ROUTES: Routes = [
   {
     path: "",
-    component: ProductsComponent
+    redirectTo: "products",
+    pathMatch: 'full' 
   },
   {
     path: "cart",
     component: CartComponent
+  },
+  {
+    path: "products",
+    component: ProductsComponent
+  },
+  {
+    path: "products/:id",
+    component: SingleProductComponent
   }
 ];
 
 @NgModule({
   imports: [CommonModule, RouterModule.forChild(ROUTES), HttpClientModule],
   providers: [ShopService],
-  declarations: [CartComponent, ProductsComponent]
+  declarations: [CartComponent, ProductsComponent, SingleProductComponent]
 })
 export class ShopModule {}
